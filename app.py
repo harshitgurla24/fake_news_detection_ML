97bf4390025ba92ec3f8043d77750fb3ad900df1
harshitgurla24 <harshitgurla24@navgurukul.org>
2026-01-08 14:14:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle
+import re
+
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+app = Flask(__name__)
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\W', ' ', text)
+    return text
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = ""
+    if request.method == "POST":
+        news = request.form["news"]
+        cleaned = clean_text(news)
+        vector = vectorizer.transform([cleaned])
+        prediction = model.predict(vector)
+
+        result = " 🔴 Fake News" if prediction[0] == 1 else "🟢 Real News"
+        print(result)
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ train_model.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import re
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+# Load dataset
+data = pd.read_csv("fake_news_dataset.csv")
+
+print(data.columns)
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\W', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text
+
+data["text"] = data["text"].apply(clean_text)
+
+X = data["text"]
+y = (data["label"] == "real").astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+vectorizer = TfidfVectorizer(stop_words="english")
+X_train_vec = vectorizer.fit_transform(X_train)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train_vec, y_train)
+
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+
+print("✅ Model & Vectorizer saved successfully!")
